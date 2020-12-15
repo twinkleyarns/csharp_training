1ec72a5de869708a7f51e0cfd7fe87aa78a1303d
antoninad <antonina.dudka@arcadia.spb.ru>
2020-12-15 17:46:09 +0300

    Задание №21**: Реализовать тесты для создания новой учётной записи в Mantis

---
+++ mantis_tests/mantis_tests/appmanager/RegistrationHelper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using System.Text.RegularExpressions;
+
+namespace mantis_tests
+{
+    public class RegistrationHelper : HelperBase
+    {
+        public RegistrationHelper(ApplicationManager manager) : base(manager) { }
+
+        public void Register(AccountData account)
+        {
+            OpenMainPage();
+            OpenRegistrationForm();
+            FillRegistrationForm(account);
+            SubmitRegistration();
+            String url = GetConfirmationUrl(account);
+            FillPasswordForm(url, account);
+            SubmitPasswordForm();
+        }
+
+        private void SubmitPasswordForm()
+        {
+            driver.FindElement(By.XPath("//button/span")).Click();
+        }
+
+        private void FillPasswordForm(String url, AccountData account)
+        {
+            driver.Url = url;
+            driver.FindElement(By.Name("password")).SendKeys(account.Password);
+            driver.FindElement(By.Name("password_confirm")).SendKeys(account.Password);
+        }
+
+        private string GetConfirmationUrl(AccountData account)
+        {
+            String message = manager.Mail.GetLastMail(account);
+            Match match = Regex.Match(message, @"http://\S*");
+            return match.Value;
+        }
+
+        private void OpenRegistrationForm()
+        {
+            driver.FindElement(By.CssSelector("a.back-to-login-link.pull-left")).Click();
+        }
+
+        public void SubmitRegistration()
+        {
+            driver.FindElement(By.XPath("//input[@type='submit']")).Click();
+        }
+
+        public void FillRegistrationForm(AccountData account)
+        {
+            driver.FindElement(By.Name("username")).SendKeys(account.Name);
+            driver.FindElement(By.Name("email")).SendKeys(account.Email);
+
+        }
+
+        public void OpenMainPage()
+        {
+            manager.Driver.Url = "http://localhost:10080/mantisbt-2.24.2/login_page.php";
+        }
+    }
+}
---
+++ mantis_tests/mantis_tests/tests/AccountCreationTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using NUnit.Framework;
+using System.IO;
+
+namespace mantis_tests
+{
+    [TestFixture]
+    public class AccountCreationTests : TestBase
+    {
+        [SetUp]
+        public void SetUpConfig()
+        {
+            app.Ftp.BackupFile("/config/config_inc.php");
+            using (Stream localFile = File.Open("config_inc.php", FileMode.Open))
+            {
+                app.Ftp.Upload("/config/config_inc.php", localFile);
+            }
+        }
+
+        [Test]
+        public void TestAccountRegistration()
+        {
+            AccountData account = new AccountData()
+            {
+                Name = "testuser0",
+                Password = "password",
+                Email = "testuser0@localhost.localdomain"
+            };
+
+            app.James.Delete(account);
+            app.James.Add(account);
+
+            app.Registration.Register(account);
+        }
+
+        [TearDown]
+        public void RestoreConfig()
+        {
+            app.Ftp.ResoreBackupFile("/config/config_inc.php");
+        }
+    }
+}
